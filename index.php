7a58631c20f8a1135a912e51c8561c17d3a13014
Nzingani <isabelnzingani@outlook.com>
2018-07-26 10:22:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exercice3</title>
+  </head>
+  <body>
+    <p>
+      <?php
+ $age = 22;
+ $gender = 'men';
+
+ if($age >= 18 && $gender == 'men')
+ {
+   echo 'vous êtes majeur et vous êtes un homme';
+ }
+ elseif($age <= 18 && $gender == 'men')
+ {
+   echo 'vous êtes mineur et vous êtes un homme';
+ }
+ ?>
+</p>
+
+<p>
+<?php
+$age = 21;
+$gender = 'women';
+
+ if($age >= 18 && $gender == 'women')
+ {
+   echo 'vous êtes majeur et vous êtes une femme';
+ }
+ elseif($age <= 18 && $gender == 'women')
+ {
+   echo 'vous êtes mineur et vous êtes une femme';
+ }
+ ?>
+</p>
+</body>
